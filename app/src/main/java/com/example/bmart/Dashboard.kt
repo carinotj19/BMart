21b7046e1d574432baacb363cab81fcd30726b1d
iMilkyP <67515859+iMilkyP@users.noreply.github.com>
2023-10-10 20:56:52 +0800

    Hamburger + bottom navbar

--- app/src/main/java/com/example/bmart/Dashboard.kt
+++ app/src/main/java/com/example/bmart/Dashboard.kt
@@ -2,38 +2,126 @@ package com.example.bmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.example.bmart.databinding.ActivityDashboardBinding
 import com.example.bmart.fragments.Cart
 import com.example.bmart.fragments.Home
+import com.example.bmart.fragments.Messages
 import com.example.bmart.fragments.Search
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class Dashboard : AppCompatActivity() {
-    private lateinit var binding : ActivityDashboardBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(Home())
-        binding.bottomNavigationView.setOnItemSelectedListener {
+        setContentView(R.layout.activity_dashboard)
 
-            when(it.itemId){
-                R.id.home -> replaceFragment(Home())
-                R.id.search -> replaceFragment(Search())
-                R.id.cart -> replaceFragment(Cart())
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.hamburgerNavigationView)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.nav_bar_open,
+            R.string.nav_bar_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-                else -> {
+        // Set the "Home" fragment as the default screen
+        if (savedInstanceState == null) {
+            replaceFragment(Home())
+            bottomNavigationView.selectedItemId = R.id.home // Highlight the "Home" item in the bottom navigation
+        }
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.profile -> {
+                    Toast.makeText(applicationContext, "Clicked Profile", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.favourites -> {
+                    Toast.makeText(applicationContext, "Clicked favourites", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.orders -> {
+                    Toast.makeText(applicationContext, "Clicked orders", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.help -> {
+                    Toast.makeText(applicationContext, "Clicked help", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.privacy -> {
+                    Toast.makeText(applicationContext, "Clicked privacy", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.terms -> {
+                    Toast.makeText(applicationContext, "Clicked terms", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.logOut -> {
+                    Toast.makeText(applicationContext, "Clicked logOut", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
                 }
             }
-            true
+            drawerLayout.closeDrawer(GravityCompat.START)
+            false
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    replaceFragment(Home())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.search -> {
+                    replaceFragment(Search())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.cart -> {
+                    replaceFragment(Cart())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.messages -> {
+                    replaceFragment(Messages())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
         }
     }
 
-    private fun replaceFragment(fragment: Fragment) {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-        fragmentTransaction.commit()
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
     }
 }
